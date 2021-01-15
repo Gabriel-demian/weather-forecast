976299e2a32e88898d650d9815bcb3ef6f85a038
Gabriel <gabriel.leoni.um@gmail.com>
2021-01-15 16:49:47 -0300

    Se empieza a generar la lógica de negocio en el servicio.
    Se genera una iteración para generar los datos según la cantidad de días ingresados.
    Se genera el método sunIsInside para verificar que el sol se encuentre dentro de los 3 planetas cuando generan un triangulo.
    El método triangleContainsSun que verifica si el clima va a ser lluvioso o nulo.
    El método planetsAligned que verifica que los planetas estén alineados si el area del triangulo es igual a cero 0

--- src/main/java/com/meli/weatherforecast/service/impl/WheatherForecastService.java
+++ src/main/java/com/meli/weatherforecast/service/impl/WheatherForecastService.java
@@ -5,6 +5,8 @@ import java.awt.geom.Point2D;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.meli.weatherforecast.enums.WeatherEnum;
+import com.meli.weatherforecast.geometry.Calculator;
 import com.meli.weatherforecast.model.SolarSystem;
 import com.meli.weatherforecast.repository.ForecastRepository;
 
@@ -18,7 +20,26 @@ public class WheatherForecastService {
 	@Autowired
 	private ForecastRepository forecastRepository;
 	
+	private Calculator calculator;
+	
 	private SolarSystem solarSystem;
+	
+	public void runForecast(int totalDays) {
+		
+		log.info("Starting to calculate and persist the forecast for the next " + totalDays + " days.");
+		
+		for (int currentDay = 0; currentDay < totalDays; currentDay++) {
+			
+			//crear pronóstico por dia.
+					
+			forecastRepository.save(null);
+			
+		}
+		
+		
+	}
+
+	
 	
 	public boolean sunIsInside(int day) {
 		
@@ -26,11 +47,55 @@ public class WheatherForecastService {
 		Point2D positionPlanet2 = solarSystem.getPlanet2Position(day);
 		Point2D positionPlanet3 = solarSystem.getPlanet3Position(day);
 		
+		Double planetsArea = calculator.areaByHeron(positionPlanet1, positionPlanet2, positionPlanet3);
+		
+		double areaWithTheSun1 = calculator.areaByHeron(SolarSystem.SUN, positionPlanet2, positionPlanet3);
+		double areaWithTheSun2 = calculator.areaByHeron(positionPlanet1, SolarSystem.SUN, positionPlanet3);
+		double areaWithTheSun3 = calculator.areaByHeron(positionPlanet1, positionPlanet2, SolarSystem.SUN);
+		
+		Double sumAreasWithTheSun = areaWithTheSun1 + areaWithTheSun2 + areaWithTheSun3;
 		
 		
-		return true;
+		return sumAreasWithTheSun.equals(planetsArea);
+	}
+	
+	public WeatherEnum triangleContainsSun(int day) {
+		
+		return sunIsInside(day) ? WeatherEnum.RAIN :  WeatherEnum.NONE;
+		
+	}
+	
+	public boolean planetsAligned(int day) {
+		
+		Double triangleArea = calculator.areaByHeron(solarSystem.getPlanet1Position(day), solarSystem.getPlanet2Position(day), solarSystem.getPlanet3Position(day));
+		
+		return triangleArea.equals(0);
 	}
 	
 	
 	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
